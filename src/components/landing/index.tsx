6008030e66f80f5a32de3b10354ff50f18812334
Jonas Mota <100393626+JonsMota@users.noreply.github.com>
2026-08-16 02:23:37 +0000

    feat: cria estrutura base da landing page

---
+++ src/components/landing/index.tsx
@@ -0,0 +1,15 @@
+import Page from '../template/Page'
+import Header from './header'
+import AdCard from './adCard'
+import Footer from './footer'
+
+
+export default function Landing() {
+  return (
+    <Page>
+      <Header />  
+      <AdCard />
+      <Footer />
+    </Page>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Landing from "@/components/landing"
+
 export default function Home() {
-  return <div>Classified</div>
+  return <Landing />
 }
