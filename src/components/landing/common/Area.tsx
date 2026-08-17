56e2831b58de5115ea75f678cb84b0a5565e4cdd
Jonas Mota <100393626+JonsMota@users.noreply.github.com>
2026-08-17 19:52:58 +0000

    feat: cria area

---
+++ src/components/landing/common/Area.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface AreaProps {
+  children: React.ReactNode
+  className?: string
+  id?: string
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  ${(props) => props.className};
+`
+
+const ContentContainer = styled.div`
+  padding-left: 1.75rem;
+  padding-right: 1.75rem;
+  width: 100%;
+
+  @media (min-width: 1280px) {
+    padding-left: 0;
+    padding-right: 0;
+    width: 1200px;
+  }
+
+   @media (min-width: 1600px) {
+    padding-left: 0;
+    padding-right: 0;
+    width: 1500px;
+  }
+`
+
+const Area: React.FC<AreaProps> = ({ children, className, id }) => {
+  return (
+    <Container id={id} className={className}>
+      <ContentContainer>{children}</ContentContainer>
+    </Container>
+  )
+}
+
+export default Area
