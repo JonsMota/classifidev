234dd5cf3e5cf849f8bad630e76d44f77ce577d6
Jonas Mota <100393626+JonsMota@users.noreply.github.com>
2026-08-17 21:51:47 +0000

    feat: cria Footer

--- src/components/landing/footer/index.tsx
+++ src/components/landing/footer/index.tsx
@@ -1,5 +1,46 @@
+import styled from 'styled-components'
+
+import Area from '../common/Area'
+
+const StyledFooter = styled.div`
+  background-color: ${(props) => props.theme.secondary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  margin-top: auto;
+`
+
+const StyledContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 60%;
+  margin: auto;
+  margin-top: 16px;
+`
+const StyledText = styled.h3`
+  color: ${(props) => props.theme.white};
+  font-weight: 300;
+  font-style: italic;
+  font-size: 20px;
+`
+const Span = styled.span`
+  color: ${(props) => props.theme.span};
+  font-weight: 700;
+`
+
 export default function Footer() {
   return (
-    <div></div>
+    <StyledFooter>
+      <Area>
+        <StyledContent>
+          <StyledText>
+            Criado por <Span>J</Span>onas <Span>M</Span>ota ® {new  Date().getFullYear()}
+          </StyledText>
+        </StyledContent>
+      </Area>
+    </StyledFooter>
   )
 }
