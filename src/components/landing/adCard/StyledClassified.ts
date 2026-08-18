3a27b8aa1815c051990d783081ad9d54a4bb6250
Jonas Mota <100393626+JonsMota@users.noreply.github.com>
2026-08-18 02:04:27 +0000

    feat: cria conjubnto de interface

---
+++ src/components/landing/adCard/StyledClassified.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Name = styled.h2`
+  font-family: Inter;
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 43.57px;
+  text-align: left;
+`
+
+export const PostDate = styled.p`
+  font-family: Inter;
+  font-size: 14px;
+  font-style: italic;
+  font-weight: 300;
+  line-height: 16.94px;
+  text-align: left;
+`
+
+export const Category = styled.p`
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 16.94px;
+  text-align: left;
+`
+
+export const Price = styled.p`
+  font-family: Inter;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 24.2px;
+  text-align: left;
+`
+
+export const Description = styled.p`
+  font-family: Inter;
+  font-size: 14px;
+  font-style: italic;
+  font-weight: 300;
+  line-height: 16.94px;
+  text-align: left;
+`
--- src/components/landing/adCard/index.tsx
+++ src/components/landing/adCard/index.tsx
@@ -1,7 +1,64 @@
+import styled from 'styled-components'
+
 import Area from '../common/Area'
+import falseClassified from '@/data/constants/falseClassified'
+import {
+  Name,
+  PostDate,
+  Price,
+  Description,
+  Category
+} from '@/components/landing/adCard/StyledClassified'
+
+const StyledAdCard = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr); /* Default mobile */
+  gap: 15px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 30px;
+    margin-top: 100px;
+    margin-bottom: 100px;
+    padding: 20px;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+    margin-top: 150px;
+    margin-bottom: 150px;
+    padding: 30px;
+  }
+
+  @media (min-width: 1280px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+  }
+
+  @media (min-width: 1600px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 70px;
+  }
+`
 
 export default function AdCard() {
   return (
-    <Area></Area>
+    <Area>
+      <StyledAdCard>
+        {falseClassified.map((item, index) => (
+          <div key={index}>
+            <Name>{item.name}</Name>
+            <PostDate>Postado em {new Date(item.date).toLocaleDateString()}</PostDate>
+            <Price>{item.price}</Price>
+            <Description>{item.description}</Description>
+            <Category>{item.category}</Category>
+          </div>
+        ))}
+      </StyledAdCard>
+    </Area>
   )
 }
